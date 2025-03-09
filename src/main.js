1df8b0d57a2b0d6f18753cc15aef21e2fb3f2bb2
SahabatCoding <sahabatcosding29@gmail.com>
2025-03-09 11:28:30 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import web from "./application/web.js";
 
-web.get('/',(req, res)=> {
-    res.send('Hello World')
-});
-
 web.listen(3000,()=>{
-    console.log(`Test`)
+    console.log(`http://localhost:3000/`)
 });
