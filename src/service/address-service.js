04df70ff8f8ee6e668c9da997fb44e973b4353d8
SahabatCoding <sahabatcoding29@gmail.com>
2025-04-16 20:27:06 +0700

    update contact

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -4,6 +4,24 @@ import { createAddressValidation } from "../validation/address-validation.js"
 import { getContactValidation } from "../validation/contact-validation.js"
 import { validate } from "../validation/validation.js"
 
+const checkContactMustExists = async(user, contactId) =>{
+    contactId = validate(getContactValidation)
+
+    const countContactId = await prismaClient.contact.count({
+        where :{
+            id : contactId,
+            username : user.username
+        }
+    })
+
+    if (countContactId !== 1){
+        throw new ResponseErorr(401, 'contact is not found')
+    }
+
+    return contactId
+
+}
+
 const create = async (user, contactId, req) =>{
     contactId = validate(getContactValidation)
     const address = validate(createAddressValidation)
