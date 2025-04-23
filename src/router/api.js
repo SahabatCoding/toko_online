6a15b51e6c0409e1ecbdfe223128e93a206a0b3a
SahabatCoding <sahabatcoding29@gmail.com>
2025-04-23 20:03:22 +0700

    create get address

--- src/router/api.js
+++ src/router/api.js
@@ -7,16 +7,21 @@ import addressController from '../controller/address-controller.js'
 const userRouter = new express.Router()
 userRouter.use(authMiddleware)
 
+//api user
 userRouter.get('/api/users/current', userController.get)
 userRouter.patch('/api/users/current', userController.update)
 userRouter.delete('/api/users/current/logout', userController.logout)
 
+//api contact
 userRouter.post('/api/contacts', contactController.create)
 userRouter.get('/api/contacts/:contactId', contactController.get)
 userRouter.put('/api/contacts/:contactId', contactController.update)
 userRouter.delete('/api/contacts/:contactId',contactController.remove)
 
+//contact address
 userRouter.post('/api/contacts/:contactId/addresses', addressController.create)
+userRouter.get('api/contacts/:contactId/addresses/:addressId')
+
 export{
     userRouter
 }
